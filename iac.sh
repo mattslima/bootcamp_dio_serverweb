ed7575e10778606af2d2f7df8b0ca1aab7cc6df7
mattslima <matheus.lima220@gmail.com>
2022-10-08 20:02:49 +0000

    apache2

---
+++ iac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update && upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+unzip main.zip
+
+cd linux-site-dio-main
+
+cp -r * /var/www/html
+
+echo "tudo ok"
+
